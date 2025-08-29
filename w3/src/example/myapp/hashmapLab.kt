1f6a17f5a7963ee3f66b919d27189b10d56cde23
iinj14 <pallyferby@gmail.com>
2025-08-29 21:32:09 +0700

    Classes and Objects

---
+++ w3/src/example/myapp/hashmapLab.kt
@@ -0,0 +1,9 @@
+package example.myapp
+
+fun main() {
+    val scientific = hashMapOf("guppy" to "poecilia reticulata", "catfish" to "corydoras", "zebra fish" to "danio rerio" )
+//    println (scientific.get("guppy"))
+//    println(scientific.get("zebra fish"))
+//    println(scientific.get("swordtail"))
+//    println(scientific.getOrDefault("swordtail", "sorry, I don't know"))
+}
